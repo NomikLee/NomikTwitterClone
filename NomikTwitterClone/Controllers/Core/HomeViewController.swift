cf041dec997f0b2a0f78955c2216364a4cc8a9d8
NomikLee <nomiklee@icloud.com>
2024-04-29 21:21:17 +0800

    add 11

--- NomikTwitterClone/Controllers/Core/HomeViewController.swift
+++ NomikTwitterClone/Controllers/Core/HomeViewController.swift
@@ -31,7 +31,7 @@ class HomeViewController: UIViewController {
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 11
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
